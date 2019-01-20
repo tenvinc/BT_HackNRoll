7d23cb8d7393871819fb776f8e69a5d1c7d0dc22
Saif Uddin Mahmud <saifum@u.nus.edu>
2019-01-20 10:32:44 +0600

    Bluno Beetle v1.1 Code
    firmware v1.97

---
+++ BLE_HacknRoll.ino
@@ -0,0 +1,19 @@
+#define BUZZER 4
+
+int incomingByte;
+
+void setup() {
+  pinMode(BUZZER, OUTPUT); 
+  Serial.begin(115200); 
+}
+
+void loop() {
+    //if we get 1 as a message, turn on tone 
+    if (Serial.available()) {
+       incomingByte = Serial.read();
+       if (incomingByte == '1') {
+          tone(BUZZER, 1500, 20000);  
+          Serial.write("Ringing\n");
+       }
+    }
+}
